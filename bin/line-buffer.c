991d23c8ed641940025b14856439048313e4d4e0
Eric Schulte <eric.schulte@gmx.com>
2011-12-27 17:45:57 -0700

    improved line-buffer for writing output to a file

--- bin/line-buffer.c
+++ bin/line-buffer.c
@@ -2,21 +2,43 @@
 #include <string.h>
 #define MAX_LINE_SIZE 1024
 
-int main(){
+int main(int argc, char *argv[]){
   char buffer[MAX_LINE_SIZE];
   int i, current_length, last_length = 0;
+  FILE * fd;
+  if(argc > 1)
+    fd = fopen(argv[1], "w+");
+
   while(gets(buffer)){
     // just in case the line was >MAX_LINE_SIZE
     buffer[MAX_LINE_SIZE] = 0;
+
+    // ensure this line is as long as the last
     current_length = strlen(buffer);
-    // wipe out remaining characters from the previous line
     for(i=current_length;i<last_length;i++)
       buffer[i] = ' ';
     buffer[i]=0;
-    printf("\r%s", buffer);
-    fflush(stdout);
+
+    // print to output s.t. only the last line exists there
+    if(argc > 1){
+      // print to a file
+      rewind(fd);
+      fprintf(fd, buffer);
+      fflush(fd);
+    }else{
+      // print to STDOUT
+      printf("\r%s", buffer);
+      fflush(stdout);
+    }
     last_length = current_length;
   }
-  puts("");
+
+
+  // ensure that after all the output ends in a newline
+  if(argc > 1)
+    fprintf(fd, "\n");
+  else
+    puts("");
+
   return 0;
 }
